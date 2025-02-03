8add1ddb2aac68820c389c091a5dd7ea7ac4eb8a
zetdou <zyziupoz@gmail.com>
2025-02-03 20:08:40 +0100

    Slider under header v1

---
+++ src/scripts/index.js
@@ -0,0 +1,20 @@
+let currentIndex = 0;
+const contents = document.querySelectorAll('.slider-content');
+const contentsArr = [...contents];
+const maxIndex = contentsArr.length - 1;
+
+function nextImage() {
+    currentIndex++;
+
+    if(currentIndex > maxIndex) {
+        currentIndex = 0;
+    }
+
+    contentsArr.forEach(function(content){
+        content.classList.remove('active');
+    })
+
+    contentsArr[currentIndex].classList.add('active');
+}
+
+setInterval(nextImage, 6000);
