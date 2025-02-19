c0c267acc61656cc053f6bdf6cacc8d842e277db
zetdou <zyziupoz@gmail.com>
2025-02-19 21:35:16 +0100

    Center modal

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,30 +1,33 @@
 let currentIndex = 0;
-const contents = document.querySelectorAll('.slider-content');
+const contents = document.querySelectorAll(".slider-content");
 const contentsArr = [...contents];
 const maxIndex = contentsArr.length - 1;
 
 function startSetInterval() {
-    setInterval(function nextImage() {
-        currentIndex++;
+  setInterval(
+    (function nextImage() {
+      currentIndex++;
 
-        if(currentIndex > maxIndex) {
-            currentIndex = 0;
-        }
+      if (currentIndex > maxIndex) {
+        currentIndex = 0;
+      }
 
-        contentsArr.forEach(function(content){
-            content.classList.remove('active');
-        })
+      contentsArr.forEach(function (content) {
+        content.classList.remove("active");
+      });
 
-        contentsArr[currentIndex].classList.add('active');
-        return nextImage;
-    }(), 8000)
-};
+      contentsArr[currentIndex].classList.add("active");
+      return nextImage;
+    })(),
+    8000
+  );
+}
 
 startSetInterval();
 
 let originClass = "header";
 let scrollClass = "headerScroll";
-let scrollTrigger = "100";
+let scrollTrigger = 250;
 
 // With add inline style not by class
 // const header = document.querySelector("header");
@@ -36,23 +39,22 @@ let scrollTrigger = "100";
 //         header.style.removeProperty("background");
 //     }
 
-
-window.onscroll = function() {
-    if(window.scrollY >= scrollTrigger) {
-        document.querySelector("header").classList.add(scrollClass);
-        document.querySelector("header").classList.remove(originClass);
-     } else {
-        document.querySelector("header").classList.add(originClass);
-        document.querySelector("header").classList.remove(scrollClass);        
-    }
+window.onscroll = function () {
+  if (window.scrollY >= scrollTrigger) {
+    document.querySelector("header").classList.add(scrollClass);
+    document.querySelector("header").classList.remove(originClass);
+  } else {
+    document.querySelector("header").classList.add(originClass);
+    document.querySelector("header").classList.remove(scrollClass);
+  }
 };
 
-const openModalBtn = document.querySelectorAll('.openCartModal')
-const modal = document.querySelector('[modalOpen]');
+const openModalBtn = document.querySelectorAll(".openCartModal");
+const modal = document.querySelector("[modalOpen]");
 
-openModalBtn.forEach(btn => {
-    btn.addEventListener('click', toggleModal)
+openModalBtn.forEach((btn) => {
+  btn.addEventListener("click", toggleModal);
 });
 function toggleModal() {
-    modal.classList.toggle('isHidden');
+  modal.classList.toggle("isHidden");
 }
