db8a3c64f89227a45251806c8b1ddc987f60459d
zetdou <zyziupoz@gmail.com>
2025-04-07 20:45:52 +0200

    productsDb.js
    Create funcionality to get the stored items from db to html render

--- src/scripts/productsDb.js
+++ src/scripts/productsDb.js
@@ -2,14 +2,35 @@ import axios from "axios";
 
 const productsDb = process.env.DB_BASE_URL;
 
-const getProducts = async () => {
+const fetchProducts = async () => {
   try {
     const res = await axios.get(productsDb);
-    console.log(res.data);
-    return res.data;
+    console.log(res.data); //podgląd w konsoli odpowiedzi z bazy danych api
+    const products = res.data;
+    const shopList = document.querySelector(".shop-list");
+    shopList.innerHTML = "";
+
+    products.forEach((element) => {
+      const productShoppingCart = document.createElement("li");
+      productShoppingCart.classList.add("shop-list-item");
+
+      productShoppingCart.innerHTML = `
+        <a href="#">
+          <img class="item-image" height="320px" width="320px" src="${element.image}" alt="${element.name}" />
+          <div class="shop-list-info">
+            <p>${element.category}</p>
+            <h3>${element.name}</h3>
+            <button class="openCartModal">Wybierz opcje</button>
+          </div>
+        </a>
+      `;
+
+      shopList.appendChild(productShoppingCart);
+    });
   } catch (err) {
     console.error("Error fetching products: ", err);
     throw err;
   }
 };
-getProducts();
+
+document.addEventListener("DOMContentLoaded", fetchProducts);
